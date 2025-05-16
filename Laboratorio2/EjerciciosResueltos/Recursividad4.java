bd74260ee52c606e5d6ff78c8e5bdec1d352a0ba
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-05-15 23:22:38 -0500

    Usando recursividad, parecido al anterior, imprime los numeros pero del 1 al 5

---
+++ Laboratorio2/EjerciciosResueltos/Recursividad4.java
@@ -0,0 +1,14 @@
+package Laboratorio2.EjerciciosResueltos;
+
+public class Recursividad4 {
+    void imprimir(int x) {
+        if (x>0) {
+            imprimir(x-1);
+            System.out.println(x);
+        }
+    }   
+    public static void main(String[] ar) {
+        Recursividad4 re=new Recursividad4();
+        re.imprimir(5);
+    }
+}
