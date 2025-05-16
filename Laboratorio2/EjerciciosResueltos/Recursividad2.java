a2543573616bdc0d4478e3623b9f40dd3669acc9
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-05-15 23:14:04 -0500

    Implementando un metodo recursivo el cual resta un entero menos 1, dandonos todos los numeros menores al entero ingresado

---
+++ Laboratorio2/EjerciciosResueltos/Recursividad2.java
@@ -0,0 +1,12 @@
+package Laboratorio2.EjerciciosResueltos;
+
+public class Recursividad2 {
+    void imprimir(int x) {
+        System.out.println(x);
+        imprimir(x-1);
+    }
+    public static void main(String[] ar) {
+        Recursividad2 re=new Recursividad2();
+        re.imprimir(5);
+    }
+}
