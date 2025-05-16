06e70e9f6010a6c83323210da1899afa9bdf3b75
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-05-15 23:18:10 -0500

    Este metodo recursivo imprime de forma descendente del 5 al 1

---
+++ Laboratorio2/EjerciciosResueltos/Recursividad3.java
@@ -0,0 +1,14 @@
+package Laboratorio2.EjerciciosResueltos;
+
+public class Recursividad3 {
+    void imprimir(int x) {
+        if (x>0) {
+            System.out.println(x);
+            imprimir(x-1);
+        }
+    }
+    public static void main(String[] ar) {
+        Recursividad3 re=new Recursividad3();
+        re.imprimir(5);
+    }
+}
