acecec8c7040489adf8af27b169b016d20e010fa
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-05-15 23:10:02 -0500

    Aprendiendo a como implementar un metodo recursivo

---
+++ Laboratorio2/EjerciciosResueltos/Recursividad1.java
@@ -0,0 +1,12 @@
+package Laboratorio2.EjerciciosResueltos;
+
+public class Recursividad1 {
+    void repetir() {
+        repetir();
+    }
+
+    public static void main(String[] ar) {
+        Recursividad1 re=new Recursividad1();
+        re.repetir();
+    }
+}
