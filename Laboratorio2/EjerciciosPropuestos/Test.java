4a4bc2b952e72a6d123017dbe8328726289201f8
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-05-16 22:24:15 -0500

    Se comprueba la funcionalidad en nuestro test

--- Laboratorio2/EjerciciosPropuestos/Test.java
+++ Laboratorio2/EjerciciosPropuestos/Test.java
@@ -2,10 +2,17 @@ package Laboratorio2.EjerciciosPropuestos;
 
 public class Test {
     public static void main(String[] args) {
-        int[] nums = {5,9,7,8};
+        int[] nums = {5,9,7,8,1};
         Propuesto1 i = new Propuesto1();
+        System.out.println("Ejercicio1:");
         i.imprimir(nums);
         i.invertirArray(nums);
         i.imprimir(nums);
+
+        int[] nums2 = {1,2,3,4,5};
+        System.out.println("Ejercicio2:");
+        Propuesto2 inv = new Propuesto2();
+        int[] result = inv.rotarIzquierdaArray(nums2);
+        i.imprimir(result);
     }
 }
