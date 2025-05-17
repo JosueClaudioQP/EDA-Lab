46887936ed8886c1e50e8c62a3262a11b346b541
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-05-16 21:54:05 -0500

    Se crea la clase Test para instanciar las clases donde se encuentran nuestros ejercicios propuestos y de esta manera probarlos en consola

---
+++ Laboratorio2/EjerciciosPropuestos/Test.java
@@ -0,0 +1,11 @@
+package Laboratorio2.EjerciciosPropuestos;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] nums = {5,9,7,8};
+        Propuesto1 i = new Propuesto1();
+        i.imprimir(nums);
+        i.invertirArray(nums);
+        i.imprimir(nums);
+    }
+}
