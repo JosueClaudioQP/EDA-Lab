483802b5de59625847649e5c60b7a70a58679a0f
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-06-02 12:05:36 -0500

    Crear una lista enlazada utilizando la librería java.util que implemente el cambio de elemento usando el método set()

---
+++ Laboratorio4/GFG3.java
@@ -0,0 +1,14 @@
+package Laboratorio4;
+import java.util.LinkedList;
+
+public class GFG3 {
+    public static void main(String[] args) {
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("Uno");
+        ll.add("Dos");
+        ll.add(1, "Tres");
+        System.out.println("Initial LinkedList " + ll);
+        ll.set(1, "Cuatro");
+        System.out.println("Updated LinkedList " + ll);
+    }
+}
