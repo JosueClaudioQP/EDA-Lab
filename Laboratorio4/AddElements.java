88ac08078208d61cbb951bea84fd64a93e1d4423
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-06-02 11:58:11 -0500

    Usando LinkedList de java para crear una lista enlazada con 5 elementos

---
+++ Laboratorio4/AddElements.java
@@ -0,0 +1,18 @@
+package Laboratorio4;
+import java.util.LinkedList;
+
+public class AddElements {
+    // Metodo principal
+    public static void main(String[] args) {
+    // Creando unaLinkedList
+    LinkedList<String> l = new LinkedList<String>();
+    // Añadiendo los elementos a la LinkedList usando el método add()
+    l.add("Uno");
+    l.add("Dos");
+    l.add("Tres");
+    l.add("Cuatro");
+    l.add("Cinco");
+    // Imprimiendo la LinkedList
+    System.out.println(l);
+    }
+}
