c2a2eca3ad901b6f3f353da626bef6172006f6f4
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-06-02 12:02:36 -0500

    Crear una lista enlazada utilizando la librería java.util que implemente el añadido de elementos, de letras del abecedario de la A a la E y también el borrado de elementos, por posición , por dato, que remueva el primero y el ultimo

---
+++ Laboratorio4/GFG.java
@@ -0,0 +1,22 @@
+package Laboratorio4;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) {
+        // Creando el objeto de la
+        // clase lista enlazada
+        LinkedList<String> ll = new LinkedList<String>();
+        // Añadido de elementos a la lista enlazada
+        ll.add("A");
+        ll.add("B");
+        ll.addLast("C");
+        ll.addFirst("D");
+        ll.add(2, "E");
+        System.out.println(ll);
+        ll.remove("B");
+        ll.remove(3);
+        ll.removeFirst();
+        ll.removeLast();
+        System.out.println(ll);
+    }
+}
