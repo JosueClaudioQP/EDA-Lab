eb5dcc1491b65014833a6cc39971fa46cff33bb2
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-06-12 15:19:37 -0500

    Se implementa la clase BST generica, esta hereda de la interfaz Comparable la cual nos permitira comparar el tipo de dato que vayamos a ingresar. Se crea un nodo root el cual será la raíz de nuestro árbol junto con su constructor vacío. Se añaden los diferentes metodos genericos que se usaran para construir y analizar nuestro arbol

---
+++ Laboratorio6/ProblemasResueltos/BST.java
@@ -0,0 +1,24 @@
+package ProblemasResueltos;
+
+public class BST <T extends Comparable<T>>{
+    public Nodo<T> root;
+    
+    public BST(){
+        this.root = null;
+    }
+
+    /*
+    public void destroy(){}
+    public void isEmpty(){}
+    public T insert(T x){}
+    public T remove(T x){}
+    public T search(T x){}
+    public void Min(){}
+    public void Max(){}
+    public void Predecesor(){}
+    public void Sucesor(){}
+    public void InOrder(){}
+    public void PostOrder(){}
+    public void PreOrder(){}
+    */
+}
