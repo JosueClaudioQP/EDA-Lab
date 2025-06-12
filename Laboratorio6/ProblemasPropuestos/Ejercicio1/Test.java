26fee6aa6977dc7df8595119ac000b0d90bd1f8c
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-06-12 16:02:50 -0500

    Se realiza la prueba correspondiente en el Test, se ingresan numeros a nuestro arbol e identifica si existe un dato duplicado

---
+++ Laboratorio6/ProblemasPropuestos/Ejercicio1/Test.java
@@ -0,0 +1,14 @@
+package ProblemasPropuestos.Ejercicio1;
+
+public class Test {
+    public static void main(String[] args) {
+        BST<Integer> arbolBST = new BST<>();
+
+        arbolBST.insert(2);
+        arbolBST.insert(5); 
+        arbolBST.insert(7);
+        arbolBST.insert(3);
+        arbolBST.insert(4);
+        arbolBST.insert(3);
+    }
+}
