6de5f9e3cd0df54fca2935606731f19325997b5b
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-06-12 20:00:11 -0500

    Se implementó el metodo generico

--- Laboratorio6/ProblemasPropuestos/Ejercicio1/BST.java
+++ Laboratorio6/ProblemasPropuestos/Ejercicio1/BST.java
@@ -15,11 +15,13 @@ public class BST<T extends Comparable<T>> {
         Nodo<T> nodo = actual;
 
         if(actual == null){
+            System.out.println("Se ingreso numero: " + x);
             nodo = new Nodo<T>(x);
         } else {
             int resC = actual.data.compareTo(x);
             if (resC == 0){ 
                 System.out.println("Dato duplicado");
+                return actual;
             }
             if (resC < 0){
                 nodo.rigth = insertNode(x, actual.rigth);
@@ -30,8 +32,24 @@ public class BST<T extends Comparable<T>> {
         return nodo; 
     }
 
+    public void destroy(){
+        destroyTree(root);
+        this.root = null;
+        System.out.println("Arbol destruido");
+    }
+
+    protected void destroyTree(Nodo<T> nodo){
+        if(nodo == null) return;
+
+        destroyTree(nodo.rigth);
+        destroyTree(nodo.left);
+
+        nodo.rigth = null;
+        nodo.left = null;
+        nodo.data = null;
+    }
+    
     /*
-    public void destroy(){}
     public void isEmpty(){}
     public T remove(T x){}
     public T search(T x){}
