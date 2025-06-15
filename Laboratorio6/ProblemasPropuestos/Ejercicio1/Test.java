121279a64dfa1c98b5a22e812efba66a1f3bbd1b
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2025-06-14 21:09:06 -0500

    Se hace las pruebas respectivas del los ordenamientos Pre y Post

--- Laboratorio6/ProblemasPropuestos/Ejercicio1/Test.java
+++ Laboratorio6/ProblemasPropuestos/Ejercicio1/Test.java
@@ -15,42 +15,56 @@ public class Test {
         arbol.insert(60);
         arbol.insert(80);
 
-        System.out.println(arbol.isEmpty());
+        System.out.println(arbol.isEmpty() + "\n");
 
         int opcion;
         do {
             System.out.println("\n--- MENU BST ---");
             System.out.println("1. Mostrar Árbol (Inorden)");
-            System.out.println("2. Eliminar un nodo");
-            System.out.println("3. Encontrar un nodo");
-            System.out.println("4. Salir");
+            System.out.println("2. Mostrar Árbol (Postorden)");
+            System.out.println("3. Mostrar Árbol (Preorden)");
+            System.out.println("4. Eliminar un nodo");
+            System.out.println("5. Encontrar un nodo");
+            System.out.println("6. Destruir árbol");
+            System.out.println("7. Salir");
             System.out.print("Elige una opción: ");
             opcion = sc.nextInt();
 
             switch (opcion) {
                 case 1:
-                    System.out.print("Recorrido Inorden: ");
+                    System.out.println("Recorrido Inorden: ");
                     arbol.InOrder();
                     System.out.println();
                     break;
                 case 2:
+                    System.err.println("Recorrido PostOrden: ");
+                    arbol.PostOrder();
+                    break;
+                case 3:
+                    System.err.println("Recorrido PreOrden: ");
+                    arbol.PreOrder();
+                    break;
+                case 4:
                     System.out.print("Ingresa el valor a eliminar: ");
                     int valor = sc.nextInt();
                     arbol.remove(valor);
                     System.out.println("Nodo eliminado (si existía).");
                     break;
-                case 3:
+                case 5:
                     System.out.println("Ingresar el valor a buscar: ");
                     int valorIng = sc.nextInt();
                     arbol.search(valorIng);
                     break;
-                case 4:
+                case 6:
+                    arbol.destroy();
+                    break;
+                case 7:
                     System.out.println("Saliendo...");
                     break;
                 default:
                     System.out.println("Opción inválida.");
             }
-        } while (opcion != 4);
+        } while (opcion != 7);
 
         sc.close();
     }
